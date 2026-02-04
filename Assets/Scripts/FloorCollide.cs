b052db8d3463b22c841f47c07477e6cae8f237c6
Markus Luik <ml9179@nyu.edu>
2026-02-04 12:06:09 -0500

    Initial Commit

---
+++ Assets/Scripts/FloorCollide.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class FloorCollide : MonoBehaviour {
+
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField] private float forceValue = 1.0f;
+    private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void ChangeColor() {
+        spriteRenderer.color = new Color(Random.value, Random.value, Random.value);
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        ChangeColor();
+        col.rigidbody.AddForce(Vector2.up * forceValue, ForceMode2D.Impulse);
+    }
+}
